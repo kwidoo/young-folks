2b3b6402d17cff46b3702d1e6b2dcabb23a2013b
Oleg Pashkovsky <oleg@pashkovsky.me>
2022-09-08 20:15:06 +0300

    Fix2

--- app/Http/Webhooks/YoungFolksHandler.php
+++ app/Http/Webhooks/YoungFolksHandler.php
@@ -11,16 +11,18 @@ class YoungFolksHandler extends WebhookHandler
     public function hi(): void
     {
         info('callback', [$this->data]);
-        $this->chat->html('Hi there!');
+        $this->chat->html('Hi there!')->send();
     }
 
     protected function handleChatMessage(Stringable $text): void
     {
         info('chat message', [$text]);
+        $this->chat->html('Hi there1!')->send();
     }
 
     protected function handleInlineQuery(InlineQuery $inlineQuery): void
     {
         info('inline query', [$inlineQuery]);
+        $this->chat->html('Hi there!2')->send();
     }
 }
