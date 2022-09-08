653de459141d4778d593f913f734ba8ec33479bd
Oleg Pashkovsky <oleg@pashkovsky.me>
2022-09-08 20:10:36 +0300

    Fix

--- app/Http/Webhooks/YoungFolksHandler.php
+++ app/Http/Webhooks/YoungFolksHandler.php
@@ -3,11 +3,18 @@
 namespace App\Http\Webhooks;
 
 use DefStudio\Telegraph\Handlers\WebhookHandler;
+use Illuminate\Support\Stringable;
 
 class YoungFolksHandler extends WebhookHandler
 {
     public function hi(): void
     {
-        $this->reply('Hello World!');
+        info('callback', [$this->data]);
+        $this->chat->html('Hi there!');
+    }
+
+    protected function handleChatMessage(Stringable $text): void
+    {
+        info('chat message', [$text]);
     }
 }
