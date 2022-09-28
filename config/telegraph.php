2e1d4a8741bafca610bfc2791cb7088b3b6bae8c
Oleg Pashkovsky <oleg@pashkovsky.me>
2022-09-28 21:25:00 +0300

    fjx

--- config/telegraph.php
+++ config/telegraph.php
@@ -15,7 +15,7 @@ return [
      * Sets Telegraph messages default parse mode
      * allowed values: html|markdown
      */
-    'default_parse_mode' => Telegraph::PARSE_HTML,
+    'default_parse_mode' => Telegraph::PARSE_MARKDOWN,
 
     /*
      * Sets the handler to be used when Telegraph
